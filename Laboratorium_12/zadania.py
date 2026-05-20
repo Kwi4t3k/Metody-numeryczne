2bca6d026e2156f6076c8fc4efbacf3300885570
Kinga <kingakwiatkowskaszkola@gmail.com>
2026-05-20 17:00:33 +0200

    lab 12 zadania (sprawdzic)

---
+++ Laboratorium_12/zadania.py
@@ -0,0 +1,177 @@
+# zad 1 ------------------------------
+def schemat_Hornera(wspolczynniki, z):
+    p = wspolczynniki[0]
+
+    for i in range(1, len(wspolczynniki)):
+        # print(p) # kroki
+        p = p * z + wspolczynniki[i]
+
+    return p
+
+# wspolczynniki = [3,2,-1,5]
+# z = complex(2, 0)
+
+# wynik = schemat_Hornera(wspolczynniki, z)
+# print(wynik)
+
+# zad 2 --------------------------
+
+def schemat_Hornera_pochodne(wspolczynniki, z):
+    p = wspolczynniki[0]
+    dp = 0
+    ddp = 0
+
+    for i in range(1, len(wspolczynniki)):
+        ddp = ddp * z + 2 * dp
+        dp = dp * z + p
+        p = p * z + wspolczynniki[i]
+
+    return p, dp, ddp
+
+# zad 3 --------------------------
+
+import cmath
+
+def metoda_laguerre_jeden_pierwiastek(wspolczynniki, z0, epsilon=1e-9, max_iteracji=100):
+    z = complex(z0)
+    n = len(wspolczynniki) - 1
+
+    for k in range(max_iteracji):
+        P, P_prim, P_2prim = schemat_Hornera_pochodne(wspolczynniki, z)
+
+        if abs(P) < epsilon:
+            return z
+        
+        G = P_prim / P
+        H = G**2 - P_2prim / P
+
+        pierwiastek = cmath.sqrt((n - 1) * (n * H - G**2))
+
+        mianownik_plus = G + pierwiastek
+        mianownik_minus = G - pierwiastek
+
+        if abs(mianownik_plus) > abs(mianownik_minus):
+            mianownik = mianownik_plus
+        else:
+            mianownik = mianownik_minus
+
+        if abs(mianownik) == 0:
+            z = z + complex(epsilon, epsilon)
+            continue
+
+        a = n / mianownik
+
+        z_nowe = z - a
+
+        if abs(a) < epsilon:
+            return z_nowe
+        
+        z = z_nowe
+
+    return z
+
+# zad 4 --------------------------
+
+def deflacja(wspolczynniki, pierwiastek):
+    nowe_wspolczynniki = [complex(wspolczynniki[0])]
+
+    for i in range(1, len(wspolczynniki) - 1):
+        nowy = wspolczynniki[i] + pierwiastek * nowe_wspolczynniki[-1]
+        nowe_wspolczynniki.append(nowy)
+
+    reszta = wspolczynniki[-1] + pierwiastek * nowe_wspolczynniki[-1]
+
+    return nowe_wspolczynniki, reszta
+
+def metoda_laguerre_wszystkie_pierwiastki(wspolczynniki, z0=0, epsilon=1e-9):
+    aktualny_wielomian = wspolczynniki.copy()
+    pierwiastki = []
+
+    while len(aktualny_wielomian) > 2:
+        pierwiastek = metoda_laguerre_jeden_pierwiastek(
+            aktualny_wielomian,
+            z0,
+            epsilon
+        )
+
+        pierwiastki.append(pierwiastek)
+
+        aktualny_wielomian, reszta = deflacja(aktualny_wielomian, pierwiastek)
+
+    ostatni_pierwiastek = -aktualny_wielomian[1] / aktualny_wielomian[0]
+    pierwiastki.append(ostatni_pierwiastek)
+
+    return pierwiastki
+
+# zad 5 --------------------------
+
+def testuj_wielomian(nazwa, wspolczynniki):
+    print("\n" + "=" * 60)
+    print(nazwa)
+    print("Współczynniki:", wspolczynniki)
+
+    z = complex(2, 0)
+
+    print("\nZadanie 1 - wartość wielomianu w punkcie z = 2:")
+    print("P(2) =", schemat_Hornera(wspolczynniki, z))
+
+    print("\nZadanie 2 - wartość P(2), P'(2), P''(2):")
+    P, P_prim, P_2prim = schemat_Hornera_pochodne(wspolczynniki, z)
+    print("P(2)   =", P)
+    print("P'(2)  =", P_prim)
+    print("P''(2) =", P_2prim)
+
+    print("\nZadanie 3 - jeden pierwiastek metodą Laguerre'a:")
+    jeden_pierwiastek = metoda_laguerre_jeden_pierwiastek(wspolczynniki, complex(0, 0))
+    print("Pierwiastek =", jeden_pierwiastek)
+
+    print("\nZadanie 4 - wszystkie pierwiastki metodą Laguerre'a:")
+    pierwiastki = metoda_laguerre_wszystkie_pierwiastki(wspolczynniki, complex(0, 0))
+
+    for i in range(len(pierwiastki)):
+        print(f"z{i + 1} =", pierwiastki[i])
+
+
+wielomian_A = [1, -6, 11, -6]
+wielomian_B = [1, -6, 11, -1]
+
+wielomian_C = [
+    39205740,
+    -147747493,
+    173235338,
+    2869080,
+    -158495872,
+    118949888,
+    -28016640
+]
+
+wielomian_D = [
+    39205740,
+    -147747493,
+    173235338,
+    2869080,
+    -158495872,
+    118949888,
+    -28016639
+]
+
+
+testuj_wielomian(
+    "a) w(x) = x^3 - 6x^2 + 11x - 6",
+    wielomian_A
+)
+
+testuj_wielomian(
+    "b) w(x) = x^3 - 6x^2 + 11x - 1",
+    wielomian_B
+)
+
+testuj_wielomian(
+    "c) przykład ze strony 27 wykładu",
+    wielomian_C
+)
+
+testuj_wielomian(
+    "d) przykład ze strony 27 wykładu + 1",
+    wielomian_D
+)
