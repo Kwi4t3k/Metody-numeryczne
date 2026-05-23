4407832182899abbd2af3ee895ab755119cb81dd
Kinga <kingakwiatkowskaszkola@gmail.com>
2026-05-23 18:11:46 +0200

    lab 12 zadania + notatki

--- Laboratorium_12/zadania.py
+++ Laboratorium_12/zadania.py
@@ -32,7 +32,7 @@ def schemat_Hornera_pochodne(wspolczynniki, z):
 
 import cmath
 
-def metoda_laguerre_jeden_pierwiastek(wspolczynniki, z0, epsilon=1e-9, max_iteracji=100):
+def metoda_laguerre_jeden_pierwiastek(wspolczynniki, z0, epsilon=1e-6, max_iteracji=100):
     z = complex(z0)
     n = len(wspolczynniki) - 1
 
@@ -83,25 +83,48 @@ def deflacja(wspolczynniki, pierwiastek):
 
     return nowe_wspolczynniki, reszta
 
-def metoda_laguerre_wszystkie_pierwiastki(wspolczynniki, z0=0, epsilon=1e-9):
+def pierwiastki_stopnia_drugiego(wspolczynniki):
+    a = wspolczynniki[0]
+    b = wspolczynniki[1]
+    c = wspolczynniki[2]
+
+    delta = b**2 - 4 * a * c
+
+    z1 = (-b + cmath.sqrt(delta)) / (2 * a)
+    z2 = (-b - cmath.sqrt(delta)) / (2 * a)
+
+    return z1, z2
+
+def metoda_laguerre_wszystkie_pierwiastki(wspolczynniki, z0=0, epsilon=1e-6):
+    pierwotny_wielomian = wspolczynniki.copy()
     aktualny_wielomian = wspolczynniki.copy()
     pierwiastki = []
 
-    while len(aktualny_wielomian) > 2:
-        pierwiastek = metoda_laguerre_jeden_pierwiastek(
-            aktualny_wielomian,
-            z0,
-            epsilon
-        )
+    while len(aktualny_wielomian) > 3:
+        pierwiastek = metoda_laguerre_jeden_pierwiastek(aktualny_wielomian, z0, epsilon)
+
+        pierwiastek = metoda_laguerre_jeden_pierwiastek(pierwotny_wielomian, pierwiastek, epsilon)
 
         pierwiastki.append(pierwiastek)
 
         aktualny_wielomian, reszta = deflacja(aktualny_wielomian, pierwiastek)
 
-    ostatni_pierwiastek = -aktualny_wielomian[1] / aktualny_wielomian[0]
-    pierwiastki.append(ostatni_pierwiastek)
+    z1, z2 = pierwiastki_stopnia_drugiego(aktualny_wielomian)
+
+    pierwiastki.append(z1)
+    pierwiastki.append(z2)
 
     return pierwiastki
+
+# uładnienie wyniku
+def ladnie(z, epsilon=1e-8):
+    if abs(z.imag) < epsilon:
+        return z.real
+
+    if abs(z.real) < epsilon:
+        return complex(0, z.imag)
+
+    return z
 
 # zad 5 --------------------------
 
@@ -113,28 +136,33 @@ def testuj_wielomian(nazwa, wspolczynniki):
     z = complex(2, 0)
 
     print("\nZadanie 1 - wartość wielomianu w punkcie z = 2:")
-    print("P(2) =", schemat_Hornera(wspolczynniki, z))
+    print("P(2) =", ladnie(schemat_Hornera(wspolczynniki, z)))
 
     print("\nZadanie 2 - wartość P(2), P'(2), P''(2):")
     P, P_prim, P_2prim = schemat_Hornera_pochodne(wspolczynniki, z)
-    print("P(2)   =", P)
-    print("P'(2)  =", P_prim)
-    print("P''(2) =", P_2prim)
+
+    print("P(2)   =", ladnie(P))
+    print("P'(2)  =", ladnie(P_prim))
+    print("P''(2) =", ladnie(P_2prim))
 
     print("\nZadanie 3 - jeden pierwiastek metodą Laguerre'a:")
-    jeden_pierwiastek = metoda_laguerre_jeden_pierwiastek(wspolczynniki, complex(0, 0))
-    print("Pierwiastek =", jeden_pierwiastek)
+    jeden_pierwiastek = metoda_laguerre_jeden_pierwiastek(wspolczynniki, z0=0)
+    print("Jeden pierwiastek =", ladnie(jeden_pierwiastek))
 
     print("\nZadanie 4 - wszystkie pierwiastki metodą Laguerre'a:")
-    pierwiastki = metoda_laguerre_wszystkie_pierwiastki(wspolczynniki, complex(0, 0))
+    pierwiastki = metoda_laguerre_wszystkie_pierwiastki(wspolczynniki, z0=0)
 
     for i in range(len(pierwiastki)):
-        print(f"z{i + 1} =", pierwiastki[i])
+        print(f"z{i + 1} =", ladnie(pierwiastki[i]))
 
 
+# a) w(x) = x^3 - 6x^2 + 11x - 6
 wielomian_A = [1, -6, 11, -6]
+
+# b) w(x) = x^3 - 6x^2 + 11x - 1
 wielomian_B = [1, -6, 11, -1]
 
+# c) przykład ze strony 27 wykładu
 wielomian_C = [
     39205740,
     -147747493,
@@ -145,6 +173,7 @@ wielomian_C = [
     -28016640
 ]
 
+# d) przykład ze strony 27 wykładu + 1
 wielomian_D = [
     39205740,
     -147747493,
