17ed8c3d5d56613199f56c8a74b5538dfcaa0323
Kinga <kingakwiatkowskaszkola@gmail.com>
2026-06-07 11:45:42 +0200

    Update zadania.py

--- Laboratorium_1/zadania.py
+++ Laboratorium_1/zadania.py
@@ -92,21 +92,37 @@ import numpy as np
 
 def epsilon_float32():
     eps = np.float32(1.0)
+
     while np.float32(1.0) + eps / np.float32(2.0) > np.float32(1.0):
         eps = eps / np.float32(2.0)
+
     return eps
+
 
 def epsilon_float64():
     eps = 1.0
+
     while 1.0 + eps / 2.0 > 1.0:
         eps = eps / 2.0
+
     return eps
+
 
 eps32 = epsilon_float32()
 eps64 = epsilon_float64()
 
+u32 = eps32 / np.float32(2.0)
+u64 = eps64 / 2.0
+
 print("Epsilon float32:", eps32)
-print("Epsilon float64:", eps64)
+print("Unit roundoff float32:", u32)
+
+print("Epsilon float64 / double:", eps64)
+print("Unit roundoff float64:", u64)
+
+print("Porównanie:")
+print("float32 ma dokładność około 7 cyfr dziesiętnych")
+print("float64 / double ma dokładność około 16 cyfr dziesiętnych")
 
 # -------------------- ZADANIE 8 ----------------------
 
